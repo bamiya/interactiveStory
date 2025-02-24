3204a801a374c8c302ac7db5ec22a388e97f34ec
bamiyamy <bamiyamy@gmail.com>
2025-02-24 16:17:20 +0900

    테스트 배경이미지 추가, css 추가

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/MainScreen.css';
 
 // MainScreen 컴포넌트는 두 개의 시작 버튼을 제공한다.
 // - startDefaultStory: 기본 스토리를 시작하는 함수
@@ -6,9 +7,11 @@ import React from 'react';
 function MainScreen({ startDefaultStory, startYgTestStory }) {
   return (
     <div className="main-screen">
-      <h1>untitle</h1>
-      <button onClick={startDefaultStory}>시작하기</button>
-      <button onClick={startYgTestStory}>YG 시작하기</button>
+      <h1>Rainy Night</h1>
+      <div className="button-container">
+        <button onClick={startDefaultStory}>시작하기</button>
+        <button onClick={startYgTestStory}>YG 시작하기</button>
+      </div>
     </div>
   );
 }
